5bd4cc8c60bc84d1776fa5eb58c8689d436dc8a7
gildabiscotti / gilda + biscotti <gilda110308@gmail.com>
2025-04-25 10:36:33 +0200

    file per funzioni

---
+++ funzioni.js
@@ -0,0 +1,18 @@
+import PromptSync from "prompt-sync"
+const prompt=PromptSync()
+
+export const crealista=()=>{
+
+}
+export const mostralista=()=>{
+    
+}
+export const eliminalista=()=>{
+    
+}
+export const aggiungilista=()=>{
+    
+}
+export const ricercalista=()=>{
+    
+}
